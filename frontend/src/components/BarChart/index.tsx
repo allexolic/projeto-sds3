160f7a4340bdd3eb764443d405b4e4c79200a1dc
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-05-10 11:01:48 -0300

    remove comments

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -59,19 +59,7 @@ function BarChart() {
             }
         },
     };
-    /*
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-    */
+
     return (
         <Chart
             options={{ ...options, xaxis: chartData.labels }}
